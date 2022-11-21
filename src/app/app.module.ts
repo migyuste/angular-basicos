9d8a1b1e4558bda1c255bd80357f578dc378524a
Miguel Yuste <yusaguilar@gmail.com>
2022-11-21 15:31:55 +0100

    Pasar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+// Modulos personalizados
+import { AppRouterModule } from './app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEs from "@angular/common/locales/es";
+import localeFr from "@angular/common/locales/fr";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData( localeEs );
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
